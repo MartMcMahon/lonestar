9e5ff5c7e642de7ff09cad60e7fa2afccdee9688
mart <matt@paradigmautomation.co>
2018-01-13 15:20:37 -0600

    ssl test

--- webserver.py
+++ webserver.py
@@ -5,7 +5,7 @@ from subprocess import PIPE, STDOUT
 import busstuff
 
 hostName = ""
-hostPort = 9000
+hostPort = 443
 
 '''''''''''''''''''''''
 default response data
@@ -290,6 +290,7 @@ class DataHandler():
 #bus = busstuff.Buses()
 
 serv = HTTPServer((hostName, hostPort), Server)
+serv.socket = ssl.wrap_socket(serv.socket, certfile='./fullchain.pem', server_side=True)
 print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
 
 dh = DataHandler()
